173b5e31ea1fdc4c0ca6113f0908a79f1ea5a2db
Simone <simone.pagotto.kg@gmail.com>
2021-01-19 16:14:54 +0100

    extended exceptions

--- dipendenti.php
+++ dipendenti.php
@@ -1,118 +1,179 @@
 <?php
+//estendo la classe Exception per riuscire a gestire messaggi diversi
+class NomeException extends Exception {
+                public function __construct($message=null,$code=0){
+                    echo $message;
+                }
+            }
+
+class CognomeException extends Exception {
+    public function __construct($message=null,$code=0){
+        echo $message;
+    }
+}
+
+class CfException extends Exception {
+    public function __construct($message=null,$code=0){
+        echo $message;
+    }
+}
 
 class Persona{
 
-    public $nome;
-    public $cognome;
-    public $codice_fiscale;
+    private $nome;
+    private $cognome;
+    private $codice_fiscale;
+
+    //cercare di passare meno parametri possibili
+    public function __construct($nome,$cognome,$codice_fiscale){
+
+            if(!is_string($nome)){
+                throw new NomeException('ERRORE $nome');
+            }
+
+            if(!(is_string($cognome))){
+                throw new CognomeException('ERRORE $cognome');
+            }
+
+            if(strlen($codice_fiscale)!=4){
+                throw new CfException('ERRORE $codice_fiscale');
+            }
+
+            $this->nome = $nome;
+            $this->cognome = $cognome;
+            $this->codice_fiscale = $codice_fiscale;
+        }
 
     public function to_string(){
         /* return print_r($this,true); */
         return 'nome: ' . $this->nome . ' ; cognome: ' . $this->cognome  . ' ; codice fiscale: ' . $this->codice_fiscale;
     }
-
-    function __construct($nome,$cognome,$codice_fiscale){
-        $this->nome = $nome;
-        $this->cognome = $cognome;
-        $this->codice_fiscale = $codice_fiscale;
-    }
-
 }
 
 class Impiegato extends Persona{
 
-    public $codice_impiegato;
-    public $compenso;
-    
-    function __construct($nome,$cognome,$codice_fiscale,$codice_impiegato, $compenso){
-        parent::__construct($nome,$cognome,$codice_fiscale);
-        $this->codice_impiegato = $codice_impiegato;
-        $this->compenso = $compenso;
+    private $codice_impiegato;
+    protected $compenso;
+
+    //decido di usare un array associativo per non passare 5 parametri
+    public function __construct($init_impiegato){
+
+        parent::__construct(
+            $init_impiegato['nome'],
+            $init_impiegato['cognome'],
+            $init_impiegato['codice_fiscale']
+        );
+
+        $this->codice_impiegato = $init_impiegato['codice_impiegato'];
+        $this->calcola_compenso();
+
     }
 
     public function to_string(){  
         return parent::to_string() . ' ; codice-imp: ' . $this->codice_impiegato . ' ; compenso: ' . $this->compenso;
     }
 
+    //classe astratta, una funzione è sostanzialmente lasciata vuota, 
+    //definisce cosa devono fare le classi che la estendono
     public function calcola_compenso(){
-        return $this->compenso;
+        return $this->compenso = 0;
     }
-
-
-    
-    /* public function to_string(){} */
 }
 
 class ImpiegatoSalariato extends Impiegato{
 
-    public $giorni_lavorati;
-    public $compenso_giornaliero;
+    private $giorni_lavorati;
+    private $compenso_giornaliero;
+
+    public function __construct($init_is){
+
+                parent::__construct($init_is);
+                
+                $this->giorni_lavorati = $init_is['giorni_lavorati'];
+                $this->compenso_giornaliero = $init_is['compenso_giornaliero'];
+                $this->calcola_compenso();
+        }
 
     public function calcola_compenso(){
         $this->compenso = $this->giorni_lavorati * $this->compenso_giornaliero;
         return $this->compenso;
     }
-
-    /* public function to_string(){  
-        return parent::to_string() . ' ; giorni-lavorati: ' . $this->giorni_lavorati . ' ; compenso-giornaliero: ' . $this->compenso_giornaliero;
-    } */
-
-    function __construct($nome,$cognome,$codice_fiscale,$codice_impiegato,$giorni_lavorati,$compenso_giornaliero){
-        parent::__construct($nome,$cognome,$codice_fiscale,$codice_impiegato,0);
-        
-        $this->giorni_lavorati = $giorni_lavorati;
-        $this->compenso_giornaliero = $compenso_giornaliero;
-        $this->calcola_compenso();
-    }
 }
 
 class ImpiegatoAOre extends Impiegato{
-    public $ore_lavorate;
-    public $compenso_orario;
+
+    private $ore_lavorate;
+    private $compenso_orario;
+
+    public function __construct($init_iao){
+
+                parent::__construct($init_iao);
+                
+                $this->ore_lavorate = $init_iao['ore_lavorate'];
+                $this->compenso_orario = $init_iao['compenso_orario'];
+                $this->calcola_compenso();
+        }
 
     public function calcola_compenso(){
         $this->compenso = $this->ore_lavorate * $this->compenso_orario;
         return $this->compenso;
-    }
-
-    /* public function to_string(){  
-        return parent::to_string() . ' ; giorni-lavorati: ' . $this->ore_lavorate . ' ; compenso-giornaliero: ' . $this->compenso_orario;
-    } */
-
-    function __construct($nome,$cognome,$codice_fiscale,$codice_impiegato,$ore_lavorate,$compenso_orario){
-        parent::__construct($nome,$cognome,$codice_fiscale,$codice_impiegato,0);
-        
-        $this->ore_lavorate = $ore_lavorate;
-        $this->compenso_orario = $compenso_orario;
-        $this->calcola_compenso();
-    }
+    }  
 }
 
 trait Progetto {
-    public $name='sito';
-    public $comissione=2000;
-
-    /* function __construct($nome,$commissione){
-        $this->nome=$nome;
-        $this->commissione=$commissione;
-    } */
-
-    public function commissione(){
-        return 2001;
-    }
+    private $nome_progetto;
+    protected $commissione;
 }
 
 class ImpiegatoSuCommissione extends Impiegato{
 
     use Progetto;
 
+    const PROPORZIONE = 0.8;
+
+    public function __construct($init_isc, $nome_proj, $commissione){
+
+        /* try{ */
+            parent::__construct($init_isc);
+
+            if(!is_string($nome_proj)){//condizione eccezione
+                throw new Exception ('SEI ENTRATO IN UN ECCEZIONE!!! nome_proj non è una stringa');
+            }
+
+            if(!is_numeric($commissione)){
+                throw new Exception ('SEI ENTRATO IN UN ECCEZIONE!!! commissione non è un numero');
+            }
+            
+            $this->nome_progetto = $nome_proj; 
+            $this->commissione = $commissione;
+            $this->calcola_compenso();
+
+        /* } catch (Exception $e){
+
+            //in questo modo gestisco l'eccezione impostando un default
+            //e non ottengo un fatal error
+            $this->nome_progetto = 'Non pervenuto'; 
+            $this->commissione = 0;
+            $this->calcola_compenso();
+            echo $e->getMessage();
+        } */
+
+           
+        }
+
     public function calcola_compenso(){
-        return $this->compenso=$commissione;
+        return $this->compenso = $this->commissione * self::PROPORZIONE;
+        //uso self perchè è una proprietà della classe e non dell'oggetto
+        //self rappresenta la classe
+        //dinamico--oggetto---: this->
+        // statico--classe -----: :: 
     }
 
-    function __construct($nome,$cognome,$codice_fiscale,$codice_impiegato){
-        parent::__construct($nome,$cognome,$codice_fiscale,$codice_impiegato,0);
-        $this->calcola_compenso();
+    public function fun(){
+
+        if($this->compenso < 1000){
+            throw new Exception('this is not funny');
+        }
     }
 }
 
--- index.php
+++ index.php
@@ -1,21 +1,53 @@
 <?php
 
 require_once('dipendenti.php');
+try{
+    $myPersona = new Persona('Simone',12,'SMNP1');
+}catch(NomeException $e){
+    echo $e->getMessage();
+    $myPersona = new Persona('Mario','Rossi','XXXX');
+}catch(CognomeException $e){
+    echo $e->getMessage();
+    $myPersona = new Persona('Anna','Verdi','YYYY');
+}catch(CfException $e){
+    echo $e->getMessage();
+    $myPersona = new Persona('Glenn','Ray','YXYX');
+}
 
-$myPersona = new Persona('Simone','Pagotto','SMNPGT');
 echo $myPersona->to_string();
 var_dump($myPersona); 
 
-$myImpiegato = new Impiegato('Simone','Pagotto','SMNPGT','001', 1000);
+$myImpiegato = new Impiegato([
+    'nome' => 'Simone',
+    'cognome' => 'Pagotto',
+    'codice_fiscale' => 'SMNPGT',
+    'codice_impiegato' => '001',
+    'compenso' => 1000
+]);
 echo $myImpiegato->to_string();
 var_dump($myImpiegato);
 
-$myImpiegatoSalariato = new ImpiegatoSalariato('Simone','Pagotto','SMNPGT','001',220,80);
+$myImpiegatoSalariato = new ImpiegatoSalariato([
+    'nome' => 'Simone',
+    'cognome' => 'Pagotto',
+    'codice_fiscale' => 'SMNPGT',
+    'codice_impiegato' => '001',
+    /* 'compenso' => 1000, */
+    'giorni_lavorati' => 220,
+    'compenso_giornaliero' => 80
+]);
 /* echo $myImpiegatoSalariato->to_string(); */
 var_dump($myImpiegatoSalariato);
 echo $myImpiegatoSalariato->calcola_compenso();
 
-$myImpiegatoAOre = new ImpiegatoSalariato('Simone','Pagotto','SMNPGT','001',140,5);
+$myImpiegatoAOre = new ImpiegatoAOre([
+    'nome' => 'Simone',
+    'cognome' => 'Pagotto',
+    'codice_fiscale' => 'SMNPGT',
+    'codice_impiegato' => '001',
+    'ore_lavorate' => 140,
+    'compenso_orario' => 5
+    ]);
 /* echo $myImpiegatoAOre->to_string(); */
 var_dump($myImpiegatoAOre);
 echo $myImpiegatoAOre->calcola_compenso();
@@ -23,7 +55,32 @@ echo $myImpiegatoAOre->calcola_compenso();
 /* $myProgetto = new Progetto('sito',2000);
 var_dump($myProgetto); */
 
-$myImpiegatoSuCommissione = new ImpiegatoSuCommissione('Simone','Pagotto','SMNPGT','001');
+try {
+    $myImpiegatoSuCommissione = new ImpiegatoSuCommissione([
+        'nome' => 'Simone',
+        'cognome' => 'Pagotto',
+        'codice_fiscale' => 'SMNPGT',
+        'codice_impiegato' => '001',
+        'compenso' => 1000,
+    ], 'sito',1000);
+}catch (Exception $e){
+    $myImpiegatoSuCommissione = new Impiegato([
+        'nome' => 'Simone',
+        'cognome' => 'Pagotto',
+        'codice_fiscale' => 'SMNPGT',
+        'codice_impiegato' => '001',
+        'compenso' => 1000,
+    ]
+    );
+    echo $e->getMessage();  
+}
+
+try{
+   $myImpiegatoSuCommissione->fun();
+}catch (Exception $e){
+    echo $e->getMessage();
+}
+
 /* echo $myImpiegatoAOre->to_string(); */
 var_dump($myImpiegatoSuCommissione);
 echo $myImpiegatoSuCommissione->calcola_compenso();
